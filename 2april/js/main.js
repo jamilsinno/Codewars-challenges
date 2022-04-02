d405c50a4f951640a36cc45cbd5dc28905917b67
Jamil Sinno <jamil.sinno@gmail.com>
2022-04-02 18:27:17 -0300

    final solution

--- 2april/js/main.js
+++ 2april/js/main.js
@@ -8,6 +8,7 @@
 // a1 = ["hoqq", "bbllkw", "oox", "ejjuyyy", "plmiis", "xxxzgpsssa", "xxwwkktt", "znnnnfqknaz", "qqquuhii", "dvvvwz"]
 // a2 = ["cccooommaaqqoxii", "gggqaffhhh", "tttoowwwmmww"]
 // mxdiflg(a1, a2) --> 13
+
 // Bash note:
 // input : 2 strings with substrings separated by ,
 // output: number as a string
@@ -26,4 +27,16 @@ describe("mxdiflg",function() {
 
   function mxdiflg(a1, a2) {
     // your code
+
+    //my solution -> did not pass logic test
+    //return (a1.length === 0 || a2.length === 0) ? -1 : Math.max(Math.abs(a1.length - a2.length))
+    // ended up misunderstanding the question
+
+    //working solution
+    function mxdiflg(a1, a2) {
+      if (a1.length === 0 || a2.length === 0) return -1
+      let l1 = a1.map(str => str.length)
+      let l2 = a2.map(str => str.length)
+      return Math.max(Math.max(...l1) - Math.min(...l2), Math.max(...l2) - Math.min(...l1))
+    }
 }
