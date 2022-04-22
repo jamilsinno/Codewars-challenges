ed7b7af3a358e5818a61c2a31a13a932431d6254
Jamil Sinno <jamil.sinno@gmail.com>
2022-04-22 17:16:47 -0300

    added a final kata

--- 22april/js/main.js
+++ 22april/js/main.js
@@ -139,3 +139,38 @@
 //   //...
 //   return bool ? 'Yes' : 'No'
 // }
+
+//--------------------------------------------------------------------
+
+// This time no story, no theory. The examples elow show you how to write function accum:
+
+// Examples:
+// accum("abcd") -> "A-Bb-Ccc-Dddd"
+// accum("RqaEzty") -> "R-Qq-Aaa-Eeee-Zzzzz-Tttttt-Yyyyyyy"
+// accum("cwAt") -> "C-Ww-Aaa-Tttt"
+// The parameter of accum is a string which includes only letters from a..z and A..Z.
+
+// sample tests
+
+// describe("accum",function() {
+//   it("Basic tests",function() {    
+//     Test.assertEquals(accum("ZpglnRxqenU"), "Z-Pp-Ggg-Llll-Nnnnn-Rrrrrr-Xxxxxxx-Qqqqqqqq-Eeeeeeeee-Nnnnnnnnnn-Uuuuuuuuuuu");
+//     Test.assertEquals(accum("NyffsGeyylB"), "N-Yy-Fff-Ffff-Sssss-Gggggg-Eeeeeee-Yyyyyyyy-Yyyyyyyyy-Llllllllll-Bbbbbbbbbbb");
+//     Test.assertEquals(accum("MjtkuBovqrU"), "M-Jj-Ttt-Kkkk-Uuuuu-Bbbbbb-Ooooooo-Vvvvvvvv-Qqqqqqqqq-Rrrrrrrrrr-Uuuuuuuuuuu");
+//     Test.assertEquals(accum("EvidjUnokmM"), "E-Vv-Iii-Dddd-Jjjjj-Uuuuuu-Nnnnnnn-Oooooooo-Kkkkkkkkk-Mmmmmmmmmm-Mmmmmmmmmmm");
+//     Test.assertEquals(accum("HbideVbxncC"), "H-Bb-Iii-Dddd-Eeeee-Vvvvvv-Bbbbbbb-Xxxxxxxx-Nnnnnnnnn-Cccccccccc-Ccccccccccc");
+//   })})
+
+// solution
+
+function accum(s) {
+  // your code
+  // split the word
+  
+  // capitalize the first letter, lower case the other letters
+  
+  // repeat the letter the number of times in accordance to its index
+  
+  return s.split('').map( (x, i) => x.toUpperCase() + x.toLowerCase().repeat(i) ).join('-')
+}
+
