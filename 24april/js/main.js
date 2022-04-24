877dbae8e670438f72f7848fc59644014b4ea78b
Jamil Sinno <jamil.sinno@gmail.com>
2022-04-24 15:08:54 -0300

    initialized file and solved a kata

---
+++ 24april/js/main.js
@@ -0,0 +1,19 @@
+// Write a function which converts the input string to uppercase.
+
+// sample tests
+
+// describe("Basic Tests", function(){
+//   it("should pass the basic tests", function(){
+//     Test.assertEquals(makeUpperCase("hello"), "HELLO");
+//   });
+// });
+
+// solution
+
+function makeUpperCase(str) {
+  // Code here
+  return str.toUpperCase()
+}
+
+//--------------------------------------------------------------------
+
