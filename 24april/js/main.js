94e1d963c9584ae4f490192f03e37688742e63e3
Jamil Sinno <jamil.sinno@gmail.com>
2022-04-24 16:53:57 -0300

    solved 6 katas

--- 24april/js/main.js
+++ 24april/js/main.js
@@ -145,3 +145,71 @@ function findOdd(A) {
 
 
 //--------------------------------------------------------------------
+
+// Make a function that will return a greeting statement that uses an input; your program should return, "Hello, <name> how are you doing today?".
+
+// [Make sure you type the exact thing I wrote or the program may not execute properly]
+
+// sample tests
+
+// const chai = require("chai");
+// const assert = chai.assert;
+// chai.config.truncateThreshold=0;
+
+// describe("Basic tests",() =>{
+//   it("Testing for fixed tests", () => {
+//     assert.strictEqual(greet("Ryan"), "Hello, Ryan how are you doing today?");
+//     assert.strictEqual(greet("Shingles"), "Hello, Shingles how are you doing today?");
+//   })
+// })
+
+// solution
+
+function greet(name){
+  //your code here
+  return `Hello, ${name} how are you doing today?`
+} 
+
+//--------------------------------------------------------------------
+
+// Your task is to create the functionisDivideBy (or is_divide_by) to check if an integer number is divisible by both integers a and b.
+
+// A few cases:
+
+
+// (-12, 2, -6)  ->  true
+// (-12, 2, -5)  ->  false
+
+// (45, 1, 6)    ->  false
+// (45, 5, 15)   ->  true
+
+// (4, 1, 4)     ->  true
+// (15, -5, 3)   ->  true
+
+// sample tests
+
+// const chai = require("chai");
+// const assert = chai.assert;
+// chai.config.truncateThreshold=0;
+
+// describe("Tests", () => {
+//   it("test", () => {
+//     assert.strictEqual(isDivideBy(-12, 2, -6), true);
+//     assert.strictEqual(isDivideBy(-12, 2, -5), false);
+//     assert.strictEqual(isDivideBy(45, 1, 6), false);
+//     assert.strictEqual(isDivideBy(45, 5, 15), true);
+//     assert.strictEqual(isDivideBy(4, 1, 4), true);
+//     assert.strictEqual(isDivideBy(15, -5, 3), true);
+//   });
+// });
+
+// solution
+
+function isDivideBy(number, a, b) {
+  // good luck
+  if(number % a === 0 && number % b === 0 ){
+    return true
+  }else{
+    return false
+  }
+}
