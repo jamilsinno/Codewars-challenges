bb42efcbb68ab01fa150d9100fa3332f0391d3b0
Jamil Sinno <jamil.sinno@gmail.com>
2022-04-30 21:39:30 -0300

    solved katas

--- 30april/js/main.js
+++ 30april/js/main.js
@@ -1,35 +1,106 @@
-// All of the animals are having a feast! Each animal is bringing one dish. There is just one rule: the dish must start and end with the same letters as the animal's name. For example, the great blue heron is bringing garlic naan and the chickadee is bringing chocolate cake.
+// /Your goal in this kata is to implement a difference function, which subtracts one list from another and returns the result.
 
-// Write a function feast that takes the animal's name and dish as arguments and returns true or false to indicate whether the beast is allowed to bring the dish to the feast.
+// It should remove all values from list a, which are present in list b keeping their order.
 
-// Assume that beast and dish are always lowercase strings, and that each has at least two letters. beast and dish may contain hyphens and spaces, but these will not appear at the beginning or end of the string. They will not contain numerals.
+// arrayDiff([1,2],[1]) == [2]
+// If a value is present in b, all of its occurrences must be removed from the other:
+
+// arrayDiff([1,2,2,2,3],[2]) == [1,3]
 
 // sample tests
 
-// describe("Tests", () => {
-//   it("test", () => {
-// Test.assertEquals(feast("great blue heron", "garlic naan"), true)
-// Test.assertEquals(feast("chickadee", "chocolate cake"), true)
-// Test.assertEquals(feast("brown bear", "bear claw"), false)
+// const chai = require("chai");
+// const assert = chai.assert;
+// chai.config.truncateThreshold=0;
+
+// describe("Basic tests", () => {
+//   it("Should pass Basic tests", () => {
+//     assert.deepEqual(arrayDiff([1,2], [1]), [2], "a was [1,2], b was [1]");
+//     assert.deepEqual(arrayDiff([1,2,2], [1]), [2,2], "a was [1,2,2], b was [1]");
+//     assert.deepEqual(arrayDiff([1,2,2], [2]), [1], "a was [1,2,2], b was [2]");
+//     assert.deepEqual(arrayDiff([1,2,2], []), [1,2,2], "a was [1,2,2], b was []");
+//     assert.deepEqual(arrayDiff([], [1,2]), [], "a was [], b was [1,2]");
+//     assert.deepEqual(arrayDiff([1,2,3], [1,2]), [3], "a was [1,2,3], b was [1,2]")
 //   });
 // });
 
 // solution
 
-function feast(beast, dish) {
-  //your function here
-  let firstLetterAnimal = beast.split('')
-  let firstLetterDish = dish.split('')
+function arrayDiff(a, b) {
+  //let arr = []
+//   console.log(a)
+//   for(let i = 0; i<a.length; i++){
+//     if(a[i] !== b){
+//      a.splice(i, 1)
+//     }
+//   }
+//   console.log(a)
   
-  console.log(beast)
-  console.log(dish)
-
-   if(firstLetterAnimal[0] === firstLetterDish[0] && firstLetterAnimal[firstLetterAnimal.length-1] === firstLetterDish[firstLetterDish.length-1] ){
-     return true
-   }else{
-     return false
-   }
+  return a.filter( x => !b.includes(x))
+  
+  //return a
 }
 
 //--------------------------------------------------------------------
 
+// Given two arrays a and b write a function comp(a, b) (orcompSame(a, b)) that checks whether the two arrays have the "same" elements, with the same multiplicities (the multiplicity of a member is the number of times it appears). "Same" means, here, that the elements in b are the elements in a squared, regardless of the order.
+
+// Examples
+// Valid arrays
+// a = [121, 144, 19, 161, 19, 144, 19, 11]  
+// b = [121, 14641, 20736, 361, 25921, 361, 20736, 361]
+// comp(a, b) returns true because in b 121 is the square of 11, 14641 is the square of 121, 20736 the square of 144, 361 the square of 19, 25921 the square of 161, and so on. It gets obvious if we write b's elements in terms of squares:
+
+// a = [121, 144, 19, 161, 19, 144, 19, 11] 
+// b = [11*11, 121*121, 144*144, 19*19, 161*161, 19*19, 144*144, 19*19]
+// Invalid arrays
+// If, for example, we change the first number to something else, comp is not returning true anymore:
+
+// a = [121, 144, 19, 161, 19, 144, 19, 11]  
+// b = [132, 14641, 20736, 361, 25921, 361, 20736, 361]
+// comp(a,b) returns false because in b 132 is not the square of any number of a.
+
+// a = [121, 144, 19, 161, 19, 144, 19, 11]  
+// b = [121, 14641, 20736, 36100, 25921, 361, 20736, 361]
+// comp(a,b) returns false because in b 36100 is not the square of any number of a.
+
+// Remarks
+// a or b might be [] or {} (all languages except R, Shell).
+// a or b might be nil or null or None or nothing (except in C++, COBOL, Crystal, D, Dart, Elixir, Fortran, F#, Haskell, Nim, OCaml, Pascal, Perl, PowerShell, Prolog, PureScript, R, Racket, Rust, Shell, Swift).
+// If a or b are nil (or null or None, depending on the language), the problem doesn't make sense so return false.
+
+// Note for C
+// The two arrays have the same size (> 0) given as parameter in function comp.
+
+// sample test
+
+// describe("Tests", () => {
+//   it("test", () => {
+// a1 = [121, 144, 19, 161, 19, 144, 19, 11];
+// a2 = [11*11, 121*121, 144*144, 19*19, 161*161, 19*19, 144*144, 19*19];
+// Test.assertEquals(comp(a1, a2), true);
+//   });
+// });
+
+// solution
+
+function comp(array1, array2){
+  //your code here
+  if(array1 === [] && array2 === []){
+    return true
+  }else if(array1 === [] || array2 === [] || array1 === null || array2 === null ){
+    return false
+  }
+
+  let newArray1 = array1.map( x => x*x )
+  
+  let compare1 = newArray1.sort( (a,b) => a-b)
+  let compare2 = array2.sort( (a,b) => a-b)
+
+  for(let i = 0; i<compare1.length; i++){
+    if(compare1[i] !== compare2[i]){
+      return false
+    }
+  }
+  return true
+}
