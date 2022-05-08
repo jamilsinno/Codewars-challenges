b90cc0aa44b8511debb104d9888e1806deae6cb9
Jamil Sinno <jamil.sinno@gmail.com>
2022-05-08 20:07:19 -0300

    solved 2 6 kyu katas

--- 8may/js/main.js
+++ 8may/js/main.js
@@ -238,3 +238,55 @@ function count (string) {
   console.log(obj)
   return obj
 }
+
+// --------------------------------------------------------------------------
+
+//The goal of this exercise is to convert a string to a new string where each character in the new string is "(" if that character appears only once in the original string, or ")" if that character appears more than once in the original string. Ignore capitalization when determining if a character is a duplicate.
+
+// Examples
+// "din"      =>  "((("
+// "recede"   =>  "()()()"
+// "Success"  =>  ")())())"
+// "(( @"     =>  "))((" 
+// Notes
+// Assertion messages may be unclear about what they display in some languages. If you read "...It Should encode XXX", the "XXX" is the expected result, not the input!
+
+// sample tests
+
+// const chai = require("chai");
+// const assert = chai.assert;
+// chai.config.truncateThreshold=0;
+
+// describe("Duplicate Encoder", () => {
+//   it("Testing for fixed tests", () => {
+//     assert.strictEqual(duplicateEncode("din"),"(((");
+//     assert.strictEqual(duplicateEncode("recede"),"()()()");
+//     assert.strictEqual(duplicateEncode("Success"),")())())","should ignore case");
+//     assert.strictEqual(duplicateEncode("(( @"),"))((");
+//   });
+// });
+
+// solution
+
+function duplicateEncode(word){
+  //split into array, count the letters
+  console.log(word)
+  const count = word.toLowerCase().split('').reduce( (obj, item) => {
+    if(!obj[item]){
+      obj[item] = 0 
+    }
+    obj[item]++
+    return obj
+  }, {} )
+  
+  //conditional to check if the letters appear more than once and then map original array with ( or ) 
+  const newWord = word.toLowerCase().split('').map( x => {
+    if(count[x] > 1){
+      return x = ')'
+    }else{
+      return x='('
+    }
+  }).join('')
+  console.log(newWord)
+  return newWord
+}
