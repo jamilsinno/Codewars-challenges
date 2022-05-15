1390cc5114b5e3c567150d63704180863d8e026c
Jamil Sinno <jamil.sinno@gmail.com>
2022-05-15 19:59:26 -0300

    solved solution011.js - hello world!

---
+++ js/solution011.js
@@ -0,0 +1,15 @@
+// Make a simple function called greet that returns the most-famous "hello world!".
+
+// PREP
+
+// no parameters to pass through
+
+// return the string hello world
+
+function greet(){
+    return `hello world!`
+}
+
+// example
+
+console.log(greet())
