151220312fa8bd2b06d6d26332dde0b72532d416
Jamil Sinno <jamil.sinno@gmail.com>
2022-05-24 17:41:01 -0300

    solved solution027.js - DeBug sayHello

---
+++ js/solution027.js
@@ -0,0 +1,12 @@
+// The starship Enterprise has run into some problem when creating a program to greet everyone as they come aboard. It is your job to fix the code and get the program working again!
+// PREP
+
+// Parameter is passed. Need to fix the code to return hello + name
+
+// return a string with the parameter passed.
+
+
+function sayHello(name) {
+    return `Hello, ${name}`
+  }
+
