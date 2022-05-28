a8184a27461d2f456a922e9c0d8307a849b9b37e
Jamil Sinno <jamil.sinno@gmail.com>
2022-05-28 16:58:41 -0300

    did banki questions

--- test.js
+++ test.js
@@ -2,16 +2,21 @@ const fizzBuzz = (n) => {
 
   for(let i = 1; i <= n; i++){
 
-    if(i % 5 === 0 && i % 3 === 0){
-      console.log('FizzBuzz')
-    }else if (i%5 === 0){
-      console.log('Buzz')
-    }else if(i%3 === 0){
-      console.log('Fizz')
-    }else{
-      console.log(i)
-    }
+    // if(i % 5 === 0 && i % 3 === 0){
+    //   console.log('FizzBuzz')
+    // }else if (i%5 === 0){
+    //   console.log('Buzz')
+    // }else if(i%3 === 0){
+    //   console.log('Fizz')
+    // }else{
+    //   console.log(i)
+    // }
+
+    i % 5 === 0 && i % 3 === 0 ? console.log('FizzBuzz') :
+    i % 3 === 0 ? console.log('Fizz') :
+    i % 5 === 0 ? console.log('Buzz') :
+    console.log(i)
   }
 }
 
-fizzBuzz(100)
+fizzBuzz(100) 
