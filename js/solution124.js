05fdc73737526716d968077806553a832ed5ea06
Jamil Sinno <jamil.sinno@gmail.com>
2022-09-03 10:44:47 -0300

    added solution124.js

---
+++ js/solution124.js
@@ -0,0 +1,9 @@
+function getDrinkByProfession(param){
+    return param.toLowerCase() === 'jabroni' ? 'Patron Tequila' :
+      param.toLowerCase() === 'school counselor' ? 'Anything with Alcohol' :
+      param.toLowerCase() === 'programmer' ? 'Hipster Craft Beer' :
+      param.toLowerCase() === 'bike gang member' ? 'Moonshine' :
+      param.toLowerCase() === 'politician' ? 'Your tax dollars' :
+      param.toLowerCase() === 'rapper' ? 'Cristal' :
+      'Beer' 
+  }
