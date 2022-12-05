926020bb1f9a533de05c04d65e2e69da3c571cc8
Jamil Sinno <jamil.sinno@gmail.com>
2022-12-05 12:28:37 -0400

    changed solution to include hashmap methods and got it working

--- js/solution193.js
+++ js/solution193.js
@@ -1,20 +1,39 @@
+// const orderedCount = function (text) {
+//   // Implement me!
+//   let hash = {}
+//   let letter = text.split('')
+//   let ans = []
+  
+//   for (ind in letter){
+//     if(!hash[letter[ind]]){
+//       hash[letter[ind]] = 1
+//     } else {
+//       hash[letter[ind]] += 1
+//     }
+//     console.log(hash)
+//   }
+//   console.log(hash)
+//   for (key in hash) {
+//     ans.push([key, hash[key]])
+//   }
+//   return ans
+// }
+
 const orderedCount = function (text) {
   // Implement me!
-  let hash = {}
+  let hash = new Map()
   let letter = text.split('')
   let ans = []
   
   for (ind in letter){
-    if(!hash[letter[ind]]){
-      hash[letter[ind]] = 1
+    if(!hash.has(letter[ind])){
+      hash.set(letter[ind], 1)
     } else {
-      hash[letter[ind]] += 1
+      hash.set(letter[ind], hash.get(letter[ind]) + 1)
     }
-    console.log(hash)
   }
-  console.log(hash)
-  for (key in hash) {
-    ans.push([key, hash[key]])
+  for (const key of hash.entries()) {
+    ans.push(key)
   }
   return ans
 }
