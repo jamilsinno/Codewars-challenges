78f1f2359a994d6f259cd5b9649a2aa7cddec93f
Jamil Sinno <jamil.sinno@gmail.com>
2023-03-18 20:55:00 -0300

    Solved Java codewars

---
+++ java/playingWithCubes1.java
@@ -0,0 +1,16 @@
+public class Cube{
+
+    //create a class member integers called "side"
+    private int Side;
+
+    // Get method
+    public int getSide() {
+        return Side;
+    }
+
+    //set method
+    public void setSide(int num){
+        this.Side = num;
+    }
+
+}
