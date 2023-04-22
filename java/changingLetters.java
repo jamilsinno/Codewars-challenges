9f0108ce2ec8a67ea7e798a07e76bbe73f43904a
Jamil Sinno <jamil.sinno@gmail.com>
2023-04-22 11:54:10 -0300

    solved codewars

---
+++ java/changingLetters.java
@@ -0,0 +1,22 @@
+import java.util.HashMap;
+import java.util.Map;
+
+public class Kata {
+    public static String swap(String st){
+      String inputString = new String(st);
+      Map<Character, Character> charMap = new HashMap<>();
+      
+      charMap.put('a', 'A');
+      charMap.put('e', 'E');
+      charMap.put('i', 'I');
+      charMap.put('o', 'O');
+      charMap.put('u', 'U');
+     // Your code here
+      
+      for (Map.Entry<Character, Character> entry : charMap.entrySet()) {
+          inputString = inputString.replace(entry.getKey(), entry.getValue());
+      }
+     
+     return inputString;
+    }
+}
