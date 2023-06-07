286912122d4a421369bda031a735488fb3488d83
Jamil Sinno <jamil.sinno@gmail.com>
2023-06-07 15:23:27 -0300

    solved codewars

--- java/seeYouNextHappyYear.java
+++ java/seeYouNextHappyYear.java
@@ -1,6 +1,5 @@
 public class HappyYear {
     static public int nextHappyYear(int year) {
-        // Your code is here ... See you Next Happy Year !!!
       ++year;
       while(String.valueOf(year).chars().distinct().count() != 4){
         ++year;
