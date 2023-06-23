ba807c556d0c1ef32329787e05a73ebc1fb166df
Jamil Sinno <jamil.sinno@gmail.com>
2023-06-23 17:58:13 -0300

    Solved codewars

---
+++ js/createTheRooms.js
@@ -0,0 +1,18 @@
+// Add rooms here
+var rooms = {
+  room1:{
+    name: this.name,
+    description: this.description,
+    completed: this.completed,
+  },
+  room2:{
+    name: this.name,
+    description: this.description,
+    completed: this.completed,
+  },
+  room3:{
+    name: this.name,
+    description: this.description,
+    completed: this.completed,
+  },
+}
